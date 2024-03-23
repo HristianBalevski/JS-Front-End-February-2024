1366459fabcb8adb971aaafa58980fe7575d32eb
Hristian Balevski <hb.hris.bal@gmail.com>
2024-03-23 20:51:57 +0000

    Added new solutions

--- 05.DOM Introduction/02.Exercise/06. Format the Text/solution.js
+++ 05.DOM Introduction/02.Exercise/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value;
+  const output = document.getElementById('output');
+
+  let container = [];
+  let finalResult = [];
+
+  const arrayOfSentences = input.split('.').filter(sentence => sentence.trim() !== '');
+
+  for (let i = 0; i < arrayOfSentences.length; i++) {
+    container.push(arrayOfSentences[i]);
+
+    if (container.length === 3 || i === arrayOfSentences.length - 1) {
+      const text = container.join('. ') + '.';
+      finalResult.push(`<p>${text}</p>`);
+      container = [];
+    }
+  }
+
+  output.innerHTML = finalResult.join('');
 }
